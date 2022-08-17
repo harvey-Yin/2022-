56dc7e2b25d3ca636184fa1c3e6e57d3348daad4
harvey yin <1172008090@qq.com>
2022-08-17 17:28:57 +0800

    test2

--- test1/pages/index/index.js
+++ test1/pages/index/index.js
@@ -66,10 +66,21 @@ Page({
 
   getMyInfo: function (e) {
     console.log(e.detail.userInfo)
-    let info = e.detail.userInfo;
-    this.setData({
-      src: info.avatarUrl,
-      name: info.nickName
+    // let info = e.detail.userInfo;
+
+    // this.setData({
+    //   src: info.avatarUrl,
+    //   name: info.nickName
+    // })
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          src: res.userInfo.avatarUrl,
+          name: res.userInfo.nickName
+        })
+      }
     })
-  }
+  },
 })
